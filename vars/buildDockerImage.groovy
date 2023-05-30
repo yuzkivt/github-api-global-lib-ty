2cbf4379252c76c3cc59d53e88ee7cd7e04fe2fd
Taras Yuzkiv <taras.yuzkiv@ivia.com>
2023-05-30 13:47:40 +0300

    new lib name tag full

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -1,4 +1,4 @@
-def call() {
+def call(String microservice) {
     def pom = readMavenPom file: 'pom.xml'
     sh "echo Hello 000 ${pom}"
     def shortCommit = sh(returnStdout: true, script: "git log -n 1 --pretty=format:'%h'").trim()
@@ -8,15 +8,23 @@ def call() {
     if (TicketNumber.startsWith("ICE-")) {
         def TICKET = "-${TicketNumber}"
         sh "echo Hello 333 ${TICKET}"
-        return TICKET
     } else {
         def TICKET = "ICE-XXX"
         sh "echo Hello 444 ${TICKET}"
-        return TICKET
     }
     def buildNumber = currentBuild.number
     sh "echo Hello 555 ${buildNumber}"
     def TAG = shortCommit + "-build-" + buildNumber
     sh "echo Hello 666 ${TAG}"
+
+    script {
+        docker.withRegistry('https://${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com', 'ecr:us-east-1:ecr') {
+
+            def customImage = docker.build("${microservice}:${TAG}${TICKET}")
+            //        customImage.push()
+            sh "echo Hello 777 ${microservice}:${TAG}${TICKET}"
+        }
+    }
+
     return
 }
